ddf966975c9d66903f23347ca65e0f4c8cf4722d
Paul Friedli <paul.friedli@edufr.ch>
2025-02-24 09:54:45 +0100

    v1.1 multithreaded to speedup

--- src/main/java/edu/ch/fr/emf/fri/GitHubOrganizationReposExporter.java
+++ src/main/java/edu/ch/fr/emf/fri/GitHubOrganizationReposExporter.java
@@ -11,7 +11,11 @@ import org.json.JSONObject;
 import net.lingala.zip4j.ZipFile;
 import java.io.File;
 import java.io.IOException;
-import java.text.DecimalFormat;
+import java.util.TreeSet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import org.eclipse.jgit.util.FileUtils;
 
 /**
@@ -27,18 +31,16 @@ import org.eclipse.jgit.util.FileUtils;
 public class GitHubOrganizationReposExporter {
 
     // Organisation GitHub dont on veut pomper les repos localement
-    private final static String REMOTE_ORG_NAME = "emf-info-d400";
+    private final static String REMOTE_ORG_NAME = "xxxxxxxxxxxxx";
 
     // Ton token GitHub perso (voir README.MD comment faire pour en obtenir un)
-    private final static String GITHUB_TOKEN = "ghp_xyzxyzxyzxyzxyzxyzxzxz";
+    private final static String GITHUB_TOKEN = "yyyyyyyyyyyyyyyyyyyyy";
 
     // Dossier où seront déposés les repos zippés (attention à conserver le / final
     // !)
     private final static String LOCAL_DESTINATION_FOLDER = "./downloaded_repos/";
 
     public static void main(String[] args) throws IOException, GitAPIException {
-        DecimalFormat df3 = new DecimalFormat("000");
-        OkHttpClient client = new OkHttpClient();
 
         System.out.println();
         System.out.println("=====================================================================================");
@@ -47,60 +49,123 @@ public class GitHubOrganizationReposExporter {
 
         System.out.println("Établissement de la liste des repos de [" + REMOTE_ORG_NAME + "]...");
 
+        TreeSet<RepoToExportAndZip> repositoriesToExportAndZip = new TreeSet<>();
+
+        try {
+
+            int currentPageWithinReposList = 0;
+            int amountReposFoundInThatPage;
+            do {
+                // For the moment we failed to read them
+                amountReposFoundInThatPage = 0;
+
+                // Go to next page of repos
+                currentPageWithinReposList++;
+
+                // Request to get the list of the next 1..100 repos in that organization
+                // (see https://docs.github.com/fr/rest/repos/repos?apiVersion=2022-11-28)
+                String url = GITHUB_API_BASE_URL + "/orgs/" + REMOTE_ORG_NAME + "/repos?per_page="
+                        + MAX_RETURNED_REPOS_PER_GITHUB_API_REQUEST + "&page=" + currentPageWithinReposList
+                        + "&type=all";
+                JSONArray repos = new JSONArray(doGetRequest(url, GITHUB_TOKEN));
+                amountReposFoundInThatPage = repos.length();
+
+                // Traitement de la collection reçue
+                for (int i = 0; i < amountReposFoundInThatPage; i++) {
+                    JSONObject repo = repos.getJSONObject(i);
+                    String repoName = repo.getString("name");
+                    String repoUrl = repo.getString("clone_url");
+
+                    // Ajouter à la liste
+                    repositoriesToExportAndZip.add(new RepoToExportAndZip(repoName, repoUrl));
+                }
+            } while (amountReposFoundInThatPage > 0);
+
+            System.out.println("Un total de " + repositoriesToExportAndZip.size()
+                    + " repos ont été trouvés pour l'organisation [" + REMOTE_ORG_NAME + "] !");
+        } catch (Exception e) {
+            System.out.println(
+                    "Erreur lors de la récupération des dépôts de l'organisation " + REMOTE_ORG_NAME + " : " + e);
+            e.printStackTrace();
+        }
+
+        System.out.println(
+                "Clonage et ZIP des " + repositoriesToExportAndZip.size() + " repos de [" + REMOTE_ORG_NAME + "]...");
+
+        ExecutorService executor = Executors.newFixedThreadPool(MAX_CONCURRENT_THREADS_EXPORTING_REPOS);
+        for (RepoToExportAndZip repo : repositoriesToExportAndZip) {
+            executor.submit(() -> {
+                threadedRepositoryExportationAndCompression(repo);
+            });
+        }
+        // Arrêter l'executor après la soumission de toutes les tâches
+        executor.shutdown();
+        try {
+            // Attendre que toutes les tâches soient terminées avant de continuer
+            if (!executor.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS)) {
+                System.out.println("Temps d'attente dépassé !");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Erreur avec les tâches réalisées en parallèle avec le thread pool : " + e);
+            e.printStackTrace();
+        }
+
+        System.out.println("Clonage et ZIP des repos de [" + REMOTE_ORG_NAME + "] terminé avec succès !");
+        System.out.println("=====================================================================================");
+        System.out.println();
+    }
+
+    private static final String GITHUB_API_BASE_URL = "https://api.github.com";
+    private final static int MAX_RETURNED_REPOS_PER_GITHUB_API_REQUEST = 64 + 32; // From 1 to 100
+    private final static int MAX_CONCURRENT_THREADS_EXPORTING_REPOS = 8;
+
+    private static void threadedRepositoryExportationAndCompression(RepoToExportAndZip repo) {
+
+        // Clonage du repository via JGit
+        try {
+            Git.cloneRepository()
+                    .setCredentialsProvider(new UsernamePasswordCredentialsProvider(GITHUB_TOKEN, ""))
+                    .setURI(repo.getRepoUrl())
+                    // .setURI(repoUrlWithToken)
+                    .setDirectory(new File(LOCAL_DESTINATION_FOLDER + repo.getRepoName())) // Répertoire local du repo
+                                                                                           // cloné
+                    .call()
+                    .close();
+
+            // Zippage du repository cloné local
+            ZipFolder(LOCAL_DESTINATION_FOLDER + repo.getRepoName());
+
+        } catch (GitAPIException e) {
+            System.out.println("Erreur lors du clonage du repo [" + repo.getRepoName() + "] !");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Erreur lors de la compression repo [" + repo.getRepoName() + "] !");
+            e.printStackTrace();
+        }
+
+        System.out.println("Exportation et compression du repo [" + repo.getRepoName() + "] terminée !");
+    }
+
+    private static String doGetRequest(String url, String GITHUB_TOKEN) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("https://api.github.com" + "/orgs/" + REMOTE_ORG_NAME
-                        + "/repos?per_page=1024&page=1&type=all")
-                .header("Authorization", "token " + GITHUB_TOKEN)
+                .url(url)
+                .header("Accept", "application/vnd.github+json")
+                .header("Authorization", "Bearer " + GITHUB_TOKEN)
                 .build();
 
-        // Exécution de la requête
         try (Response response = client.newCall(request).execute()) {
             if (!response.isSuccessful()) {
-                throw new IOException("Erreur dans la requête : " + response);
+                throw new IOException("Unexpected code " + response);
             }
-
-            // Récupération des données JSON
-            String jsonData = response.body().string();
-            JSONArray repos = new JSONArray(jsonData);
-            int nbreTotalRepos = repos.length();
-
-            System.out.println("Clonage et ZIP des " + nbreTotalRepos + " repos de [" + REMOTE_ORG_NAME + "]...");
-
-            // Parcours des repositories et clonage
-            for (int i = 0; i < nbreTotalRepos; i++) {
-                JSONObject repo = repos.getJSONObject(i);
-                String repoName = repo.getString("name");
-                String repoUrl = repo.getString("clone_url");
-                String strAvance = df3.format(i + 1) + "/" + df3.format(nbreTotalRepos);
-
-                System.out.print("  - " + strAvance + " [" + repoName + "] : ");
-
-                // Clonage du repository via JGit
-                System.out.print("clonage...");
-                Git.cloneRepository()
-                        .setCredentialsProvider(new UsernamePasswordCredentialsProvider(GITHUB_TOKEN, ""))
-                        .setURI(repoUrl)
-                        // .setURI(repoUrlWithToken)
-                        .setDirectory(new File(LOCAL_DESTINATION_FOLDER + repoName)) // Répertoire local du repo cloné
-                        .call()
-                        .close();
-
-                // Zippage du repository cloné local
-                ZipFolder(LOCAL_DESTINATION_FOLDER + repoName);
-
-            }
-
-            System.out.println("Clonage et ZIP des repos de [" + REMOTE_ORG_NAME + "] terminé avec succès !");
-            System.out.println("=====================================================================================");
-            System.out.println();
+            return response.body().string();
         }
     }
 
     @SuppressWarnings("resource")
     private static void ZipFolder(String pathToRepoFolder) throws IOException {
-        System.out.print(" zippage...");
         new ZipFile(pathToRepoFolder + ".zip").addFolder(new File(pathToRepoFolder));
-        System.out.println(" suppression du clone...");
         FileUtils.delete(new File(pathToRepoFolder), FileUtils.RECURSIVE + FileUtils.RETRY);
     }
 
